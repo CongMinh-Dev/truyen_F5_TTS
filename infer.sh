abca14a09f343fd1053d8143f1231ca3d64ac11d
Minh <lightstormlxag93@gmail.com>
2026-04-10 20:35:14 +0700

    chuan bi data tieng viet

--- infer.sh
+++ infer.sh
@@ -1,5 +1,5 @@
 f5-tts_infer-cli \
---model "F5TTS_v1_Base" \
+--model "F5TTS_Base" \
 --ref_audio ref.wav \
 --ref_text "cả hai bên hãy cố gắng hiểu cho nhau" \
 --gen_text "mình muốn ra nước ngoài để tiếp xúc nhiều công ty lớn, sau đó mang những gì học được về việt nam giúp xây dựng các công trình tốt hơn" \
@@ -10,3 +10,4 @@ f5-tts_infer-cli \
 # --output_dir  nếu không truyền thì nó tự tạo thư mục test ở cấp ngoài cùng, rồi lưu vào đó
 # --vocab_file data/your_training_dataset/vocab.txt \
 # --ckpt_file ckpts/your_training_dataset/model_last.pt \
+# --model "F5TTS_v1_Base" \
