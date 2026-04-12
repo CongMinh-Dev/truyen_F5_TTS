fe8152d4b14b06cc63eaf307dd5cb84d55e4bc7d
Minh <lightstormlxag93@gmail.com>
2026-04-12 15:36:11 +0700

    .

--- infer.sh
+++ infer.sh
@@ -1,5 +1,5 @@
 f5-tts_infer-cli \
---model "F5TTS_Base" \
+--model "F5TTS_v1_Base" \
 --ref_audio ref.wav \
 --ref_text "cả hai bên hãy cố gắng hiểu cho nhau" \
 --gen_text "mình muốn ra nước ngoài để tiếp xúc nhiều công ty lớn, sau đó mang những gì học được về việt nam giúp xây dựng các công trình tốt hơn" \
@@ -11,3 +11,4 @@ f5-tts_infer-cli \
 # --vocab_file data/your_training_dataset/vocab.txt \
 # --ckpt_file ckpts/your_training_dataset/model_last.pt \
 # --model "F5TTS_v1_Base" \
+# --model "F5TTS_Base" \ xài được
