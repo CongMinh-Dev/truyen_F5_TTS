45a210c1c15a65c9c73e96aef680c6eeae726ca4
Minh <lightstormlxag93@gmail.com>
2026-04-24 20:50:16 +0700

    .

--- my_server.py
+++ my_server.py
@@ -38,7 +38,7 @@ app = Flask(__name__)
 CORS(app)
 
 # --- CẤU HÌNH HỆ THỐNG ---
-MODEL_NAME = "F5TTS_Base"
+MODEL= "F5TTS_Base" #nó là tên file yaml chứ không phải tên MODEL.
 CKPT_FILE = "/content/F5-TTS-2/ckpts/model_last.pt"
 VOCAB_FILE = "/content/F5-TTS-2/data/Emilia_ZH_EN_pinyin/vocab.txt"
 vocoder_name = "vocos"
@@ -54,7 +54,7 @@ print(f"--- Đang nạp model vào {device} ---")
 vocoder = load_vocoder(vocoder_name=vocoder_name, is_local=False, device=device)
 
 # load model
-model_cfg_path = f"/content/F5-TTS-2/src/f5_tts/configs/{MODEL_NAME}.yaml"
+model_cfg_path = f"/content/F5-TTS-2/src/f5_tts/configs/{MODEL}.yaml"
 model_cfg = OmegaConf.load(model_cfg_path)
 model_cls = get_class(f"f5_tts.model.{model_cfg.model.backbone}")
 model_arc = model_cfg.model.arch
@@ -73,8 +73,11 @@ def convert_text():
         gen_text = data.get('text')
         model_name = data.get('model_name')
         file_name = f"{data.get('fileName', 'output')}.wav"
-        toc_do_doc = float(data.get('toc_do_doc', 1.0))
+        # toc_do_doc = float(data.get('toc_do_doc', 1.0))
+        toc_do_doc = float(0.8)
         suy_nghi = int(data.get('suy_nghi', 0))
+        character = data.get('character')
+
 
         ref_audio_path = f"{MP3_DIR}/{model_name}/{model_name}.MP3"
         if not os.path.exists(ref_audio_path):
@@ -82,7 +85,7 @@ def convert_text():
 
 
         # --- QUAN TRỌNG: XỬ LÝ GENERATOR ---
-        wave_path = Path(OUTPUT_DIR) / file_name
+        wave_path = str(Path(OUTPUT_DIR) / file_name) #vì path trả  về PosixPath (Một đối tượng/object). nên cần biến nó thành string
         wave_path_tam_thoi = os.path.join(OUTPUT_DIR, f"tam_{file_name}")
         ref_text = get_model_content(MP3_DIR, model_name)
         ref_audio = os.path.join(MP3_DIR, model_name, f"{model_name}.MP3")
@@ -117,7 +120,7 @@ def convert_text():
             text = re.sub(reg2, "", text)
             ref_audio_ = voices[voice]["ref_audio"]
             ref_text_ = voices[voice]["ref_text"]
-            local_speed = voices[voice].get("speed", speed)
+            local_speed = voices[voice].get("speed", toc_do_doc)
             gen_text_ = text.strip()
             print(f"Voice: {voice}")
             audio_segment, final_sample_rate, spectrogram = infer_process(
@@ -146,6 +149,8 @@ def convert_text():
 
             with open(wave_path_tam_thoi, "wb") as f: #open() mở file hoặc tạo mới file
                 #f.name chình là wave_path hay gọi là đường dẫn (vd: /content/output/mot.wav)
+                #final_wave là nội dung file âm thanh
+                #final_sample_rate hình như là 24khz, xem thêm bên utils_infer.py
                 sf.write(f.name, final_wave, final_sample_rate) 
                 print(f.name)
 
@@ -154,9 +159,9 @@ def convert_text():
         # Hậu xử lý
         if suy_nghi != 0:
             convert_voice_suy_nghi(wave_path_tam_thoi, wave_path, pan=-0.15)
-        elif model_name == "hop_thoai_may":
+        elif character == "hop_thoai_may":
             convert_voice_he_thong(wave_path_tam_thoi, f"{MP3_DIR}/sound_bg_he_thong.MP3", wave_path)
-        elif model_name == "loa_phat_thanh":
+        elif character == "loa_phat_thanh":
             convert_voice_loa_phat_thanh(wave_path_tam_thoi, "", wave_path)
         else:
             if os.path.exists(wave_path): os.remove(wave_path)
